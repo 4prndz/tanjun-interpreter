8dede88bff8ee317248a1919dc5f1c399dd4ab0f
4prndz <victorkouichi40@gmail.com>
2024-02-15 04:49:08 -0300

    feat: add support to REPL

---
+++ src/repl/repl.ts
@@ -0,0 +1,29 @@
+import readline from "readline";
+import { Lexer } from "../lexer/lexer";
+import { TokenTypes } from "../token/token";
+
+const repl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "->",
+});
+
+repl.prompt();
+
+repl.on("line", (input) => {
+  const lexer = Lexer.newLexer(input);
+
+  while (true) {
+    const token = lexer.nextToken();
+    console.log(token);
+    if (token.type === TokenTypes.EOF) {
+      break;
+    }
+  }
+  repl.prompt();
+});
+
+repl.on("close", () => {
+  console.log("END");
+  process.exit(0);
+});
