3af977777a41a1d39eec096011b24e321761093a
4prndz <victorkouichi40@gmail.com>
2024-02-14 23:19:48 -0300

    feat: finishing tokenization, ignoring whitespace, add identifiers

---
+++ src/lexer/lexer.ts
@@ -0,0 +1,94 @@
+import { Token, TokenTypes, lookupIdent } from "../token/token";
+import { isLetter, newToken } from "../helpers/helpers"
+
+export class Lexer {
+  private input: string;
+  private position: number;
+  private readPosition: number;
+  private ch: string;
+
+  constructor(input: string) {
+    this.input = input;
+    this.position = 0;
+    this.readPosition = 0;
+    this.ch = "";
+  }
+
+  public static newLexer(input: string): Lexer {
+    const lexer = new Lexer(input);
+    lexer.readChar();
+    return lexer;
+  }
+
+  private readChar() {
+    if (this.readPosition >= this.input.length) {
+      this.ch = "";
+    } else {
+      this.ch = this.input[this.readPosition];
+    }
+    this.position = this.readPosition;
+    this.readPosition++;
+  }
+
+  public nextToken(): Token {
+    let token: Token = {
+      type: TokenTypes.EOF,
+      literal: '',
+    }
+
+    this.skipWhitespace();
+
+    switch (this.ch) {
+      case '=':
+        token = newToken(TokenTypes.ASSIGN, this.ch);
+        break;
+      case ';':
+        token = newToken(TokenTypes.SEMICOLON, this.ch);
+        break;
+      case '(':
+        token = newToken(TokenTypes.LPAREN, this.ch);
+        break;
+      case ')':
+        token = newToken(TokenTypes.RPAREN, this.ch);
+        break;
+      case '{':
+        token = newToken(TokenTypes.LBRACE, this.ch);
+        break;
+      case '}':
+        token = newToken(TokenTypes.RBRACE, this.ch);
+        break;
+      case ',':
+        token = newToken(TokenTypes.COMMA, this.ch);
+        break;
+      case '+':
+        token = newToken(TokenTypes.PLUS, this.ch);
+        break;
+      default:
+        if (isLetter(this.ch)) {
+          token.literal = this.readIdentifier();
+          token.type = lookupIdent(token.literal);
+          return token;
+        } else {
+          token = newToken(TokenTypes.ILLEGAL, this.ch)
+        }
+        break;
+    }
+    this.readChar();
+    return token;
+  }
+
+  private readIdentifier(): string {
+    const position = this.position;
+    while (isLetter(this.ch)) {
+      this.readChar();
+    }
+
+    return this.input.slice(position, this.position);
+  }
+
+  private skipWhitespace() {
+    while(this.ch === ' ' || this.ch === "\t" || this.ch === "\r" || this.ch === "\n") {
+      this.readChar()
+    }
+  }
+}
---
+++ src/token/token.ts
@@ -0,0 +1,41 @@
+import { newToken } from "../helpers/helpers";
+
+export const TokenTypes = {
+  IDENT: 'IDENT',
+  INT: 'INT',
+  ASSIGN: '=',
+  PLUS: '+',
+  COMMA: ',',
+  SEMICOLON: ';',
+  LPAREN: '(',
+  RPAREN: ')',
+  LBRACE: '{',
+  RBRACE: '}',
+
+  FUNCTION: 'FUNCTION',
+  LET: 'LET',
+
+  ILLEGAL: 'ILLEGAL',
+  EOF: 'EOF',
+} as const;
+
+const keywords = {
+  fn: newToken(TokenTypes.FUNCTION, 'fn'),
+  let: newToken(TokenTypes.LET, 'let')
+} as const;
+
+export type TokenType = (typeof TokenTypes)[keyof typeof TokenTypes];
+
+export type Token = {
+  type: TokenType,
+  literal: string,
+}
+
+export function lookupIdent(ident: string): TokenType {
+  const keyword = keywords[ident as keyof typeof keywords];
+  if (keyword) {
+    return keyword.type;
+  }
+
+  return TokenTypes.IDENT
+}
